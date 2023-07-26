964150a0cd622cfd40cc0c21915712eaebc7be4a
itsmraga-hub <ragawilliam570@gmail.com>
2023-07-27 02:13:07 +0300

    Task 3: handle multiple promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,10 @@
-import createUser from "./utils";
-import uploadPhoto from "./utils";
-
+import { createUser, uploadPhoto } from './utils';
 
 export default function handleProfileSignup() {
-  
+  return Promise
+    .all([createUser(), uploadPhoto()])
+    .then((arr) => {
+      console.log(`${arr[1].body} ${arr[0].firstName} ${arr[0].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
