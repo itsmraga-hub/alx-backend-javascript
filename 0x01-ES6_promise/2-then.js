536391803e504685bfa8f1d7df292cc524bf17d5
itsmraga-hub <ragawilliam570@gmail.com>
2023-07-26 16:07:45 +0300

    task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  promise
+  return promise
     .then(() => ({ status: 200, body: 'success' }))
-    .catch(() => (Error()))
+    .catch(() => (new Error()))
     .then(() => { console.log('Got a response from the API'); });
 }
