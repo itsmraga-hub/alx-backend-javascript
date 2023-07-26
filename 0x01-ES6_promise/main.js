650fb2a7eab8fb9e44d3d3a074631e70dc0eee33
itsmraga-hub <ragawilliam570@gmail.com>
2023-07-27 02:14:22 +0300

    Make executable

--- 0x01-ES6_promise/main.js
+++ 0x01-ES6_promise/main.js
@@ -1,4 +1,9 @@
-import handleResponseFromAPI from "./2-then";
+import signUpUser from "./4-user-promise";
 
-const promise = Promise.resolve();
-handleResponseFromAPI(promise);
+console.log(signUpUser("Bob", "Dylan"));
+
+
+import uploadPhoto from './5-photo-reject';
+
+console.log(uploadPhoto('guillaume.jpg'));
+
