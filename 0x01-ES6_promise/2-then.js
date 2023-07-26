153e10b2221a367f08de49d6eab867055828660a
itsmraga-hub <ragawilliam570@gmail.com>
2023-07-26 16:03:19 +0300

    Task 2: correction

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    console.log('Got a response from the API');
-  }).catch(() => { Error(); });
+  promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => (Error()))
+    .then(() => { console.log('Got a response from the API'); });
 }
