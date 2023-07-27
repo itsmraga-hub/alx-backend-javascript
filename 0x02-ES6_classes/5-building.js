940b26308bb3ef78deaf8bd7a01a92304e0798b3
itsmraga-hub <ragawilliam570@gmail.com>
2023-07-27 14:14:47 +0300

    Task 5: Building Class

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,14 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft === 'number') {
+      this._sqft = sqft;
+    }
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
