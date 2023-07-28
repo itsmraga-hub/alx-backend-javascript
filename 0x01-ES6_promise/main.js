1d9d99c031b920c3aef1e2ae53e428edb62edad9
itsmraga-hub <ragawilliam570@gmail.com>
2023-07-28 18:48:43 +0300

    Removed main.js

--- 0x01-ES6_promise/main.js
+++
@@ -1,9 +0,0 @@
-import signUpUser from "./4-user-promise";
-
-console.log(signUpUser("Bob", "Dylan"));
-
-
-import uploadPhoto from './5-photo-reject';
-
-console.log(uploadPhoto('guillaume.jpg'));
-
