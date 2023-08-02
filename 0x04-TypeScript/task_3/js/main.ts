caee4b3690b38e1e8c0bb2a6227e69ce271c4b44
Raga <ragawilliam570@gmail.com>
2023-08-02 20:24:40 +0300

    Task 8: ambient namespaces complete

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,8 +1,12 @@
-interface Student {
-  firstName: string;
-  lastName: string;
-  age: number;
-  location: string;
-}
+/// <reference path="./crud.d.ts" />
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud';
 
-const studentsList = [{}, {}];
+const row: RowElement = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+};
+const newRowID: RowID = CRUD.insertRow(row);
+const updatedRow: RowElement = {...row, age: 23};
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
