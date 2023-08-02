648b0ae9eab1b3f85e88525fb88b2413d6612f9e
Raga <ragawilliam570@gmail.com>
2023-08-02 11:51:38 +0300

    Task 0: Create a Student interface

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -6,17 +6,17 @@ interface Student {
 }
 
 const student_1: Student = {
-  firstName: 'William';
-  lastName: 'Raga';
-  age: 25;
-  location: 'Kiambu';
+  firstName: 'William',
+  lastName: 'Raga',
+  age: 25,
+  location: 'Kiambu',
 }
 
 const student_2: Student = {
-  firstName: 'William';
-  lastName: 'Raga';
-  age: 25;
-  location: 'Kikuyu';
+  firstName: 'William',
+  lastName: 'Raga',
+  age: 25,
+  location: 'Kikuyu',
 }
 
 const studentsList: Array<Student> = [student_1, student_2];
@@ -25,25 +25,25 @@ const studentsList: Array<Student> = [student_1, student_2];
 export const displayStudents = (students: Array<Student>): void => {
   const table = document.createElement('table');
   let output = '';
+  
   for (const student of students) {
-    output += `
+    output +=`
       <tr>
         <td>${student.firstName}</td>
-	<td>${student.location}</td>
-      </tr>
-    `;
+	    <td>${student.location}</td>
+      </tr>`;
   }
   table.innerHTML = `
     <thead>
-      <th>
-        <td>FirstName</td>
-	<td>Location</td>
-      </th>
+      <tr>
+        <th>FirstName</th>
+	    <th>Location</th>
+      </tr>
     </thead>
     <tbody>
       ${output}
-    </tbody>
-  `;
+    </tbody>`;
+  console.log(table.innerHTML)
 }
 
 
